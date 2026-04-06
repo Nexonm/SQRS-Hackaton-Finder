3d59ca4ac05a561dd9c92e398b45efceef16921f
Uberch <vam.molch@gmail.com>
2026-04-06 17:53:32 +0300

    Fix(R1): Fix CC parsing

--- backend/scripts/check_cc.py
+++ backend/scripts/check_cc.py
@@ -3,6 +3,7 @@ from sys import exit
 
 result = input()
 result = json.loads(result)
-for _, declaration in result.items():
-    if declaration["type"] == "function" and declaration["complexity"] > 9:
-        exit(1)
+for _, decls in result.items():
+    for decl in decls:
+        if decl["type"] == "function" and decl["complexity"] > 9:
+            exit(1)
