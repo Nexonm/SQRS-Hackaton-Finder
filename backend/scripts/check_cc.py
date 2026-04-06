296f8f6aa5e4371ca781ee15ba46c8d6e5779596
Uberch <vam.molch@gmail.com>
2026-04-06 18:16:50 +0300

    Fix(QA): Change " -> ' to fix f-string

--- backend/scripts/check_cc.py
+++ backend/scripts/check_cc.py
@@ -8,7 +8,7 @@ flag = False
 for filename, decls in result.items():
     for decl in decls:
         if decl["type"] == "function" and decl["complexity"] > 9:
-            print(f"Exceeded CC limit in { filename }:{ decl["name"] } on line { decl["lineno"] }")
+            print(f"Exceeded CC limit in { filename }:{ decl['name'] } on line { decl['lineno'] }")
             flag = True
 
 if flag:
