01b23b327965fb7c1ef027f7e4ca682e749610f1
Uberch <vam.molch@gmail.com>
2026-04-06 18:04:20 +0300

    Impr(CI): Imporved quality gates

--- backend/scripts/check_cc.py
+++ backend/scripts/check_cc.py
@@ -3,7 +3,8 @@ from sys import exit
 
 result = input()
 result = json.loads(result)
-for _, decls in result.items():
+for filename, decls in result.items():
     for decl in decls:
         if decl["type"] == "function" and decl["complexity"] > 9:
+            print(f"Exceeded CC limit in {filename}:{decl["name"]} on line {decl["lineno"]}")
             exit(1)
--- backend/scripts/check_mi.py
+++ backend/scripts/check_mi.py
@@ -5,4 +5,5 @@ result = input()
 result = json.loads(result)
 for _, metric in result.items():
     if metric["mi"] < 65:
+        print(f"MI is below required for {filename}:{decl["name"]} on line {decl["lineno"]}")
         exit(1)
